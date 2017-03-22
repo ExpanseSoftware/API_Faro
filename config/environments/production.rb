47b7468b35e69a8ee938c557d0e321e9f97774f8
Alexander Peredo Torossova <aptpta@yahoo.es>
2017-03-22 13:06:22 -0400

    Arreglo en correos para heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { :host => "peaceful-plains-17218.herokuapp.com" }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',

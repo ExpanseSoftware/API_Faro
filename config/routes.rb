c4b01c668e9cc16a1b0d58473140433638e2dd0b
Alexander Peredo Torossova <aptpta@yahoo.es>
2017-03-22 12:56:42 -0400

    Arreglo en Root para heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users
 
+  root 'users#index'
+
   # Link to confirm an account when created
   get '/confirm_account' => 'users#confirm_account'
   post '/login' => 'users#login'
